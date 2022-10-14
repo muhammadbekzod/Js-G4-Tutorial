7c900822ecfdba77f331d2abf335c19b066edc31
Muhammad <61837014+BekzodAbdulkhaev@users.noreply.github.com>
2022-10-14 23:18:51 +0900

    Math-Tutorial

---
+++ math.js
@@ -0,0 +1,41 @@
+
+
+var a = 7.65;
+var b = 125;
+
+// Umumiy yahlidlash!
+// console.log(Math.round(a));
+
+//Biror sonni darajaga ko'tarish!
+// console.log(Math.pow(a, 10))
+
+//ildiz ostiga olish!
+// console.log(Math.sqrt(a))
+
+//Kub ildiz ostiga olish!
+// console.log(Math.cbrt(b))
+
+
+//Modulda chiqarish
+// console.log(Math.abs(a - b))
+
+//Yuqoriga yahlidlash
+// console.log("Yuqoriga yahlidlash:", Math.ceil(a))
+
+//pastga yahlidlash
+// console.log("Pastga yahlidlash: ", Math.floor(a))
+
+
+// console.log(Math.min(2, 3, 5, 13))
+// console.log(Math.max(2, 3, 5, 13))
+
+
+
+// console.log(Math.random())
+
+
+// console.log(parseInt(Math.random()))
+
+//  || or; && and;
+
+console.log(parseInt(101101, 2))
---
+++ script.js
@@ -0,0 +1,30 @@
+
+// string "asd213123asdqwdqwd"
+// number 12343532
+// boolean true/false
+// NAN not a Number
+// null 
+// Undefined
+
+
+
+var a = 10
+var b = 3
+var c = a / b
+
+console.log(a ** 2)
+// console.log(c)
+
+// console.log(Number.parseInt(c))
+
+// console.log(Number.parseFloat(c))
+
+// var d = c.toFixed(3)
+// console.log(typeof d)
+
+// console.log(typeof Number.parseFloat(d))
+
+// console.log(typeof b)
+// console.log(c.toFixed(3))
+// 1. sdufhuinee hewihriower
+// 2. siofsdoifn
