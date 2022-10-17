307ab67fb1d52910b9c0dad2c7af6043e7999550
Muhammad <61837014+BekzodAbdulkhaev@users.noreply.github.com>
2022-10-17 23:29:25 +0900

    String tutorial

---
+++ String.js
@@ -0,0 +1,51 @@
+
+age = 25;
+
+var str = "We are Be Coder group"
+
+var math = "2 + 2"
+
+
+console.log(math)
+
+console.log("Eval: ", eval(math))
+console.log(new String(math))
+
+// console.log(str.toUpperCase())
+
+// console.log(str.toLowerCase())
+
+
+// console.log(str.startsWith("we"))
+
+// console.log(str.endsWith("group"))
+
+// console.log(str.padEnd(str.length + 5, '****'))
+
+// console.log(str.padStart(str.length + 5, '****'))
+
+
+// console.log(str.replace(/coder/i, 'Developer'))
+
+//H/w
+// console.log(str.replace(/Coder/(g, i), 'Developer'))
+
+
+
+
+// console.log(str.indexOf('Group'))
+// console.log(str.lastIndexOf("are"))
+
+
+// console.log(str.includes('group'));
+// console.log(str.includes('Group'));
+
+// console.log(str.search('group'));
+
+
+
+// console.log(str.slice(3, -1))
+
+// console.log(str.substr(3, 6))
+
+// console.log(str.substring(3, -1))
